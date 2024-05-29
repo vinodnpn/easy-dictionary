a1f4e27411ebb9de8db598f30c48307cf7071dac
Vinod Neupane <vinod.npn@gmail.com>
2024-05-29 21:12:10 +0545

    Initial commit

---
+++ app/src/main/java/com/example/easydictionary/RetrofitInstance.kt
@@ -0,0 +1,19 @@
+package com.example.easydictionary
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+object RetrofitInstance {
+
+    private const val BASE_URL = "https://api.dictionaryapi.dev/api/v2/entries/"
+
+    private fun getInstance(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val dictionaryApi: DictionaryApi = getInstance().create(DictionaryApi::class.java)
+}
